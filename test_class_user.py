1bc8c5e499f1f2dff1ad555cd4988da62d013aa8
ksiemion <01187279@pw.edu.pl>
2023-12-13 00:43:16 +0100

    Defined tests for User class

--- test_class_user.py
+++ test_class_user.py
@@ -1,5 +1,10 @@
 from class_user import User
 from generate_id import generate_user_id
+import pytest
+from errors import (
+    EmptyNameError,
+    ShortPasswordError,
+)
 
 
 def test_generate_user_id(monkeypatch):
@@ -9,3 +14,22 @@ def test_generate_user_id(monkeypatch):
     id = generate_user_id()
     user = User(id, 'Jan Kowalski', 'haslo123')
     assert user.id == 2222
+
+
+def test_user():
+    id = generate_user_id()
+    user = User(id, 'Jan Kowalski', 'haslo123')
+    assert user.id == id
+    assert user.name == 'Jan Kowalski'
+    assert user.password == 'haslo123'
+    assert user.borrowed_books == []
+
+
+def test_user_empty_name():
+    with pytest.raises(EmptyNameError):
+        User(generate_user_id(), '', 'haslo123')
+
+
+def test_user_short_password():
+    with pytest.raises(ShortPasswordError):
+        User(generate_user_id(), 'Jan Kowalski', 'haslo')
