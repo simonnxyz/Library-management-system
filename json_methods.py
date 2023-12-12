aa663ceb36fabd6ba88fa7a2c9b0e8529548254e
ksiemion <01187279@pw.edu.pl>
2023-12-12 23:04:25 +0100

    Defined medhods for json files

---
+++ json_methods.py
@@ -0,0 +1,11 @@
+import json
+
+
+def read_json(file):
+    with open(file) as file_handle:
+        return json.load(file_handle)
+
+
+def write_json(file, dump_list):
+    with open(file, 'w') as file_handle:
+        json.dump(dump_list, file_handle, indent=4)
