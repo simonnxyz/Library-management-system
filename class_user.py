f2ef289bccf02793571174e2df5651c901adfe98
ksiemion <01187279@pw.edu.pl>
2023-12-13 00:28:10 +0100

    Defined getters for User class

--- class_user.py
+++ class_user.py
@@ -10,3 +10,19 @@ class User:
         self._name = name
         self._password = password
         self._borrowed_books = borrowed_books
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def password(self):
+        return self._password
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def borrowed_books(self):
+        return self._borrowed_books
