b35ce5b31b13301f1ca51dd6b69ba943f4285f80
ksiemion <01187279@pw.edu.pl>
2023-12-15 19:31:28 +0100

    Defined add_copy_of_book method

--- class_library.py
+++ class_library.py
@@ -35,10 +35,7 @@ class Library:
         write_json('books.json', self.books)
         return f'The book ({id}) has been successfully added.'
 
-    def remove_book(
-            self,
-            book_id: int, 
-            ):
+    def remove_book(self, book_id: int,):
         updated_books = []
         for book_info in self.books:
             if book_info["id"] != book_id:
@@ -48,3 +45,17 @@ class Library:
         self._books = updated_books
         write_json('books.json', self.books)
         return f'The book ({book_id}) has been successfully removed.'
+
+    def add_copy_of_book(self, book_id: int):
+        book_copy = None
+        for book_info in self.books:
+            if book_info["if"] == book_id:
+                book_copy = book_info
+        if not book_copy:
+            raise NoBookIDError(book_id)
+        title = book_copy["title"]
+        author = book_copy["author"]
+        release_year = book_copy["release_year"]
+        genre = book_copy["genre"]
+        self.add_new_book(title, author, release_year, genre)
+        return f'The copy of book ({book_id}) has been successfully added.'
