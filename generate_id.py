f47a2bc42e1de7d9eac4beb555be66ae103985f2
ksiemion <01187279@pw.edu.pl>
2023-12-15 02:37:50 +0100

    Added docstrings describing generate_id

--- generate_id.py
+++ generate_id.py
@@ -5,6 +5,9 @@ from json_methods import read_json
 
 
 def generate_id(min_range, max_range, objects):
+    """
+    Generates a random ID within the specified range that is not already in use.
+    """
     while True:
         random_id = randint(min_range, max_range)
         all_ids = [object.id for object in objects]
@@ -13,15 +16,24 @@ def generate_id(min_range, max_range, objects):
 
 
 def generate_book_id():
+    """
+    Generates a unique random book ID within the specified range.
+    """
     books = read_json('books.json')
     return generate_id(1000, 9999, [Book(**book_info) for book_info in books])
 
 
 def generate_user_id():
+    """
+    Generates a unique random user ID within the specified range.
+    """
     users = read_json('users.json')
     return generate_id(2000, 9999, [User(**user_info) for user_info in users])
 
 
 def generate_librarian_id():
+    """
+    Generates a unique random librarian ID within the specified range.
+    """
     libs = read_json('librarians.json')
     return generate_id(1000, 1999, [Librarian(**lib_info) for lib_info in libs])
