2a6649d4b288c93cb4b1c5d021ab5f45fdb6aa9d
ksiemion <01187279@pw.edu.pl>
2023-12-15 03:00:32 +0100

    Defined Library class getters

--- class_library.py
+++ class_library.py
@@ -6,3 +6,15 @@ class Library:
         self._books = read_json('books.json')
         self._users = read_json('users.json')
         self._librarians = read_json('librarians.json')
+
+    @property
+    def books(self):
+        return self._books
+
+    @property
+    def users(self):
+        return self._users
+
+    @property
+    def librarians(self):
+        return self._librarians
