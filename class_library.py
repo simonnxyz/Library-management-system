402cdc7de29e8b87efcff6aa333eff7ccebf227d
ksiemion <01187279@pw.edu.pl>
2023-12-15 02:59:17 +0100

    Defined Library class

---
+++ class_library.py
@@ -0,0 +1,8 @@
+from json_methods import read_json, write_json
+
+
+class Library:
+    def __init__(self):
+        self._books = read_json('books.json')
+        self._users = read_json('users.json')
+        self._librarians = read_json('librarians.json')
