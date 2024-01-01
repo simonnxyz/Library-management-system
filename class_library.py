d37f2e6b10d8d591dd6f46f33d816d0516d0f8e8
ksiemion <01187279@pw.edu.pl>
2024-01-01 22:32:30 +0100

    Added docstrings

--- class_library.py
+++ class_library.py
@@ -23,6 +23,10 @@ from errors import (
 
 
 class Library:
+    """
+    Library class representing a library with books, users, and librarians.
+    Reads books, users, and librarians data from JSON files.
+    """
     def __init__(self):
         self._books = read_json('books.json')
         self._users = read_json('users.json')
@@ -40,7 +44,10 @@ class Library:
     def librarians(self):
         return self._librarians
 
-    def login_role_check(self, id, password):
+    def login_role_check(self, id: int, password: str):
+        """
+        Check the login credentials for a user or librarian.
+        """
         for user_info in self.users:
             if user_info["id"] == id:
                 if user_info["password"] != password:
@@ -53,16 +60,25 @@ class Library:
                 return librarian_info
 
     def update_data(self):
+        """
+        Update library data by re-reading information from JSON files.
+        """
         self._books = read_json('books.json')
         self._users = read_json('users.json')
         self._librarians = read_json('librarians.json')
 
-    def add_new_book(self, new_book: Book):
+    def add_new_book(self, new_book: Book) -> str:
+        """
+        Add a new book to the library.
+        """
         self._books.append(new_book.__dict__())
         write_json('books.json', self.books)
         return f'The book ({new_book.id}) has been successfully added.'
 
-    def remove_book(self, book_id: int):
+    def remove_book(self, book_id: int) -> str:
+        """
+        Remove a book from the library.
+        """
         updated_books = []
         for book_info in self.books:
             if book_info["id"] != book_id:
@@ -75,7 +91,10 @@ class Library:
         write_json('books.json', self.books)
         return f'The book ({book_id}) has been successfully removed.'
 
-    def add_copy_of_book(self, book_id: int, new_id: int):
+    def add_copy_of_book(self, book_id: int, new_id: int) -> str:
+        """
+        Add a copy of an existing book to the library.
+        """
         book_copy = None
         for book_info in self.books:
             if book_info["id"] == book_id:
@@ -90,7 +109,10 @@ class Library:
         self.add_new_book(book)
         return f'The copy of book ({book_id}) has been successfully added.'
 
-    def search_book_by_keyword(self, keyword):
+    def search_book_by_keyword(self, keyword: str) -> str:
+        """
+        Search for books in the library by a keyword.
+        """
         if not keyword:
             raise NoKeywordError
         searches = []
@@ -106,12 +128,18 @@ class Library:
         return '\n'.join(searches)
 
     def available_genres(self):
+        """
+        Get a list of available genres in the library.
+        """
         genres = [book_info["genre"] for book_info in self.books]
         if not genres:
             raise GenresNotFoundError
         return genres
 
-    def search_book_by_genre(self, chosen_genre):
+    def search_book_by_genre(self, chosen_genre: str) -> str:
+        """
+        Search for books in the library by genre.
+        """
         if chosen_genre not in self.available_genres():
             raise UnavailableGenreError
         searches = []
@@ -122,12 +150,18 @@ class Library:
         return '\n'.join(searches)
 
     def available_authors(self):
+        """
+        Get a list of available authors in the library.
+        """
         authors = [book_info["author"] for book_info in self.books]
         if not authors:
             raise AuthorsNotFoundError
         return authors
 
-    def search_book_by_author(self, chosen_author):
+    def search_book_by_author(self, chosen_author: str) -> str:
+        """
+        Search for books in the library by author.
+        """
         if chosen_author not in self.available_authors():
             raise UnavailableAuthorError
         searches = []
@@ -138,12 +172,18 @@ class Library:
         return '\n'.join(searches)
 
     def available_years(self):
+        """
+        Get a list of available release years in the library.
+        """
         years = [str(book_info["release_year"]) for book_info in self.books]
         if not years:
             raise YearsNotFoundError
         return years
 
-    def search_book_by_year(self, chosen_year):
+    def search_book_by_year(self, chosen_year: str) -> str:
+        """
+        Search for books in the library by release year.
+        """
         if chosen_year not in self.available_years():
             raise UnavailableYearError
         searches = []
@@ -154,10 +194,16 @@ class Library:
         return '\n'.join(searches)
 
     def add_new_user(self, new_user: User):
+        """
+        Add a new user to the library.
+        """
         self._users.append(new_user.__dict__())
         write_json('users.json', self.users)
 
     def remove_user(self, user_id: int):
+        """
+        Remove a user from the library.
+        """
         updated_users = []
         for user_info in self.users:
             borrowed = user_info["borrowed_books"]
@@ -172,10 +218,16 @@ class Library:
         write_json('users.json', self.users)
 
     def add_new_librarian(self, new_librarian: Librarian):
+        """
+        Add a new librarian to the library.
+        """
         self._librarians.append(new_librarian.__dict__())
         write_json('librarians.json', self.librarians)
 
     def remove_librarian(self, remove_id: int, librarian_id: int):
+        """
+        Remove a librarian from the library.
+        """
         if librarian_id == remove_id:
             raise RemoveYourselfError
         updated_librarians = []
@@ -189,6 +241,9 @@ class Library:
         write_json('librarians.json', self.librarians)
 
     def get_books_stats(self):
+        """
+        Get statistics on the number of times each book has been borrowed.
+        """
         if not self.books:
             raise NoBooksError
         stats = {}
@@ -198,6 +253,9 @@ class Library:
         return stats
 
     def get_users_stats(self):
+        """
+        Get statistics on the number of books borrowed by each user.
+        """
         if not self.users:
             raise NoUsersError
         stats = {}
@@ -206,14 +264,20 @@ class Library:
             stats[f'{user.name} ({user.id})'] = len(user.borrowing_history)
         return stats
 
-    def available_books_info(self):
+    def available_books_info(self) -> str:
+        """
+        Get information on all available books in the library.
+        """
         info = []
         for book_info in self.books:
             book = Book(**book_info)
             info.append(str(book))
         return '\n'.join(info)
 
-    def users_librarians(self):
+    def users_librarians(self) -> str:
+        """
+        Get information on all users and librarians in the library.
+        """
         users_librarians = []
         for user_info in self.users:
             user = User(**user_info)
@@ -223,7 +287,10 @@ class Library:
             users_librarians.append(librarian.search_info())
         return '\n'.join(users_librarians)
 
-    def search_user(self, keyword):
+    def search_user(self, keyword) -> str:
+        """
+        Search for users or librarians in the library by a keyword.
+        """
         if not keyword:
             raise NoKeywordError
         searches = []
