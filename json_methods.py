684d306550b2b17e9231a00ae92c0ba265094361
ksiemion <01187279@pw.edu.pl>
2024-01-02 00:33:34 +0100

    Added docstrings

--- json_methods.py
+++ json_methods.py
@@ -2,10 +2,16 @@ import json
 
 
 def read_json(file):
+    """
+    Reads and parses JSON data from a file.
+    """
     with open(file) as file_handle:
         return json.load(file_handle)
 
 
 def write_json(file, dump_list):
+    """
+    Writes a list or dictionary as JSON data to a file.
+    """
     with open(file, 'w') as file_handle:
         json.dump(dump_list, file_handle, indent=4, default=str)
